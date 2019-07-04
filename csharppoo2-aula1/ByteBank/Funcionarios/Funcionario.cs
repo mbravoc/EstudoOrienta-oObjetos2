837014aa7e37ca717511e87327b7cffb55d018a9
Matheus Correa <matheuscorreabravo@gmail.com>
2019-07-04 14:54:46 -0300

    Orientação a objetos 2

---
+++ csharppoo2-aula1/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class Funcionario
+    {
+
+
+
+        public static int TotaldeFuncionarios { get; private set; } 
+
+            public string Nome { get; set; }
+            public string CPF { get; private set; }
+            public double Salario { get; protected set; }
+            public string Senha { get; set; }
+
+        public Funcionario(double salario, string cpf)
+            {
+            Console.WriteLine("Criando Funcionário");
+
+            Salario = salario;
+
+            CPF = cpf;
+            
+            TotaldeFuncionarios++;
+            }
+
+        public bool Autenticar (string senha)
+        {
+            return Senha == senha;
+        }
+        public abstract void AumentarSalario();
+
+
+        public abstract double GetBonificacao();
+
+    }
+}
---
+++ csharppoo2-aula1/ByteBank/Program.cs
@@ -0,0 +1,76 @@
+﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CalculaBonificacao();
+            UsarSistema();
+            
+            Console.ReadLine();
+
+        }
+
+        public static void UsarSistema()
+        {
+            SistemaInterno sistemaInterno = new SistemaInterno();
+            Diretor carlos = new Diretor("981.981.290..70");
+            carlos.Nome = "Carlos";
+            carlos.Senha = "123";
+
+            GerenteDeConta patricia = new GerenteDeConta("167.382.345-93");
+            patricia.Nome = "Patricia";
+            patricia.Senha = "124";
+
+            Designer tiago = new Designer("863.362.748-10");
+            tiago.Nome = "Tiago";
+
+            ParceiroComercial parceiro = new ParceiroComercial();
+
+            parceiro.Senha = "123456";
+
+            sistemaInterno.Logar(parceiro, "123456");
+            //sistemaInterno.Logar(pedro, "dsdsd");
+            sistemaInterno.Logar(carlos, "123");
+            sistemaInterno.Logar(patricia, "124");
+
+        }
+
+        public static void CalculaBonificacao()
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+
+            Diretor carlos = new Diretor("981.981.290..70");
+            carlos.Nome = "Carlos";
+
+            Designer tiago = new Designer("863.362.748-10");
+            tiago.Nome = "Tiago";
+
+            GerenteDeConta patricia = new GerenteDeConta("167.382.345-93");
+            patricia.Nome = "Patricia";
+
+            Auxiliar lucas = new Auxiliar("874.492.901-65");
+            lucas.Nome = "Lucas";
+
+            gerenciadorBonificacao.Registrar(carlos);
+            gerenciadorBonificacao.Registrar(tiago);
+            gerenciadorBonificacao.Registrar(patricia);
+            gerenciadorBonificacao.Registrar(lucas);
+
+            Console.WriteLine("TOtal de bonificações no mês:" + gerenciadorBonificacao.GetTotalBonificacao());
+
+
+
+
+        }
+    }
+}
